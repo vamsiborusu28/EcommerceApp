ada808c10143305dd5bea3e9afc189d10da44897
Vamsi Borusu <120910678+vamsiborusu28@users.noreply.github.com>
2023-04-24 18:32:05 +0000

    UserSchema methods are added

--- src/config/config.js
+++ src/config/config.js
@@ -6,6 +6,8 @@ const config={
 
     PORT: process.env.PORT || 5000,
     MONGODB_URL: process.env.MONGODB_URL || "mongodb://localhost:27017/ecomm",
+    JWT_SECRET:process.env.JWT_SECRET || "YOURSECRET",
+    JWT_EXPIRY:process.env.JWT_EXPIRY || "30d",
 }
 
 export default config;
--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -2,6 +2,11 @@
 
 import mongoose from "mongoose";
 import AuthRoles from "../utils/authRoles.js";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from './src/config/config.js';
+import crypto from 'crypto';
+
 
 const UserSchema=new mongoose.Schema(
 {
@@ -21,11 +26,51 @@ const UserSchema=new mongoose.Schema(
         minLength:[8,'Password atleast contains 8 characters'],
         select:false,
     },
-    roles:{
+    role:{
         type:String,
         enum:Object.values(AuthRoles),
         default:AuthRoles.USER,
-    }    
+    },
+    forgetPasswordToken:String,
+    forgotPasswordExpiry:Date,  
 },
 {timestamps:true}
-)
+);
+
+// Encrypting the password before saving into the database by using mongoose pre hook
+
+UserSchema.pre("save", async function(next){
+
+    if(!UserSchema.isModified("password")) return next();
+    // Encrypting the password
+    bcrypt.hash(this.password,10);
+    next();
+})
+
+
+// If we want to add methods to our schema we can add it by declaring the following
+
+UserSchema.methods={
+
+    comparePassword: async function(EnteredPassword){
+        return await bcrypt.compare(EnteredPassword,this.password);
+    },
+
+    // Generate JWT Token
+    getJWTtoken: function(){
+        // jwt.sign(payload,secretkey,options)
+        jwt.sign({_id:this._id,role:this.role},config.JWT_SECRET,{
+            expiresIn:config.JWT_EXPIRY,
+        });
+    },
+  
+    // Implementing Forget Password Token
+
+    // generateForgetPasswordToken : async function(){
+
+
+    // }
+}
+
+
+export default mongoose.models("Users",UserSchema);
