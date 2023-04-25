0cdf3f60743df0aa7d2d93bea955afe53bd336da
Vamsi Borusu <120910678+vamsiborusu28@users.noreply.github.com>
2023-04-25 09:42:13 +0000

    Login and Logout controllers are added

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 //Bring Express
 import express from 'express';
-
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 // Asssign Express
 
 const app=express();
 
+// default properties of express to make settings
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+app.use(cookieParser());
 
 // export the app
 export default app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,10 +2,20 @@ import asyncHandler from "../service/asyncHandler";
 import User from './src/models/user.schema.js'; 
 
 
+//cookieOptions
+const cookieOptions={
+    expiresIn:new Date(Date.now() + 3*24*60*60*1000),
+    httpOnly:true,
+}
+
+
 export const signup= asyncHandler( async (req,res,next) => {
 
     // get the data from web
     const {name,email,password}=req.body;
+
+    
+
 
     if(!name || !email || !password){
         throw new CustomError("Please enter all details",400);
@@ -26,12 +36,68 @@ export const signup= asyncHandler( async (req,res,next) => {
 
     // get JWT Token
     user.getJWTToken();
+    
+
+    //add token to the cookie
+    res.cookie("token",token,cookieOptions);
+    
 
     user.password=undefined;
 
     res.status(200).json({
         sucess:true,
         user,
+        token,
     })
 
 });
+
+
+export const login=asyncHandler(async (req,res,next) => {
+
+    const {email,password}=req.body;
+
+    if(!email || !password){
+        throw new CustomError("fields cannot be empty",400);
+    }
+
+    const user=User.findOne({email}).select("+password");
+
+    if(!user){
+        throw new Error("User not found",401);
+    }
+
+    const sucess=user.comparePassword(password);
+    // get jwttoken
+  
+    if(sucess){
+        const token=user.getJWTToken();
+        res.cookie("token",token,cookieOptions);
+        user.password=undefined;
+
+        res.status(200).json({
+            success:true,
+            user,
+            token,
+        });
+           
+    }
+
+    throw new CustomError("Incorrect Password",400);
+});
+
+
+export const logout=asyncHandler(async (req,res,next) => {
+    res.cookie("token",null,{
+        expiresIn:new Date(Date.now()),
+        httpOnly:true,
+    })
+
+    res.status(200).json({
+        sucess:true,
+        token,
+    })
+});
+
+
+
