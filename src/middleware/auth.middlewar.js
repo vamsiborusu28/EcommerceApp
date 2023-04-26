19f843ec2baeb90f891894b8241935fc8094ea14
Vamsi Borusu <120910678+vamsiborusu28@users.noreply.github.com>
2023-04-26 07:45:34 +0000

    Auth Middleware code is added

---
+++ src/middleware/auth.middlewar.js
@@ -0,0 +1,41 @@
+import asyncHandler from "../service/asyncHandler";
+import JWT from 'jsonwebtoken';
+import config from './config/config.js';
+import CustomError from './utils/CustomError.js';
+import User from './models/user.schema.js';
+
+
+
+export const isLoggedIn= asyncHandler(async (req,res,next) => {
+    //declare a token which is used to check whether the user is logged in the system or not
+    let token;
+    //get the token from the user cookie or from header object if exists
+    if(req.cookies.token || (req.header.authorization && (req.header.authorization.startsWith("bearer")))){
+        token=req.header.authorization.split(' ')[1];
+    }
+    
+    if(!token){
+        return new CustomError("invalid token",401);
+    }
+
+    try{
+        // Verify our JWT Token if verified get the details of the user 
+        const userPayload=JWT.verify(token,config.JWT_SECRET);
+        const userDetails=User.findOne(userPayload._id,"name email role");
+        req.userDetails=userDetails;
+        next();
+
+    }catch(error){
+        return new CustomError("Not Authorized access the resources ",401);
+    }
+
+
+});
+
+
+export const isAuthorised=(...requiredRoles) => asyncHandler( async (req,res,next) => {
+    if(!requiredRoles.includes(req.user.role)){
+        throw new CustomError("User is Not Authorized to access the resources ",400);
+    }
+    next();
+})
