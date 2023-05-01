bdc23b3aca26f44d0cc898724218eee2255cf709
Vamsi Borusu <120910678+vamsiborusu28@users.noreply.github.com>
2023-05-01 11:08:00 +0000

    Deleteproduct Controller is added

--- src/controllers/product-file.controller.js
+++ src/controllers/product-file.controller.js
@@ -6,9 +6,10 @@ import fs from "fs";
 import { FileUpload } from '../service/imageUpload.js';
 import { config } from '../config/config.js';
 import { Mongoose } from 'mongoose';
+import { FileDelete } from '../service/imageUpload.js';
 
 
-export const addProduct=asyncHandler(async(req,res))
+export const addProduct=asyncHandler(async(req,res) =>
 {
     // Import Formidable package
 
@@ -72,7 +73,53 @@ export const addProduct=asyncHandler(async(req,res))
             product,
         })
     });
-}
+});
 
+
+
+export const deleteProduct=asyncHandler( async (req,res) => {
+    const form=formidable({
+        multiples:true,
+        keepExtensions:true,
+    });
+
+    const {id:productId}=req.params;
+    const name=req.body;
+
+    // delete the data from the database
+    const product= await Product.findById({id});
+
+    if(!product){
+        throw new CustomError("Product Not Found",404);
+    }
+
+
+    const deleteProduct =Promise.all(
+
+        product.photos.map(async (element,index) => {
+            await FileDelete(
+                {
+                    bucketName:config.S3_BUCKET_NAME,
+                    key: `products/${product._id.toHexString()}/photo_${index+1}.png`,
+                }
+            );
+        })
+
+    )
+
+    //execute the delete products methods
+
+    await deleteProduct;
+    // delete the product from the database
+    await product.remove();
+
+
+    res.status(200).json({
+        success:true,
+        message:"Product has been deleted successfully",
+
+    })
+
+});
 
 
